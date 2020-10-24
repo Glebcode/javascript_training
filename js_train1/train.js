0953a5ec5e734d9d81184cbb9928e17720d9a75a
Sofiychuk Ihor <lighthollow.is@gmail.com>
2020-10-24 17:00:20 +0300

    update(train.js): add example code

---
+++ js_train1/train.js
@@ -0,0 +1,21 @@
+// let block = document.getElementById('Hello-man');
+
+
+function example(){
+    let blabla = 'war';
+    console.log("example -> blabla", blabla)
+}
+
+// example();
+
+// alert(block)
+// console.log('block', block.style.width = '100px')
+
+let x = 'loh';
+let y = "1";
+let z = `block${y}`;
+let num = 1;
+let num2 = 2;
+let num3 = num + num2;
+console.log(num3)
+// prompt('??', '')
