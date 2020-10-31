2c71ea35b366916f5a11d254922ece6dd041f0a7
Sofiychuk Ihor <lighthollow.is@gmail.com>
2020-10-31 13:07:11 +0200

    add train operator

--- js_train1/train.js
+++ js_train1/train.js
@@ -1,11 +1,11 @@
 // let block = document.getElementById('Hello-man');
 
 
-function example(){
+function example() {
     let blabla = 'war';
-    console.log("example -> blabla", blabla)
+    console.log("example -> blabla", blabla);
 }
-
+let arr1 = []
 // example();
 
 // alert(block)
@@ -14,8 +14,24 @@ function example(){
 let x = 'loh';
 let y = "1";
 let z = `block${y}`;
-let num = 1;
+let num = -10;
 let num2 = 2;
 let num3 = num + num2;
-console.log(num3)
+let m = '-6';
+let m1 = '2';
+// let res = (m - m1) + '';
+let res = +m + +m1;
+let s = "моя" + " строка";
+let counter = 1;
+counter++
+counter++
+counter++
+counter--
+counter--
+let numRes = 10 % 4; // 4 * 2 = 8; 10 - 8 = 2;
+
+
+console.log(typeof (res), res, s, counter, numRes)
+
+
 // prompt('??', '')
