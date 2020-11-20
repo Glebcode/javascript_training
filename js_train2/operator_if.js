c6034d3ac5fa77734c559bab31b75f0f709ebc9d
Sofiychuk Ihor <lighthollow.is@gmail.com>
2020-11-20 21:27:48 +0200

    add array train

--- js_train2/operator_if.js
+++ js_train2/operator_if.js
@@ -1,22 +1,30 @@
-let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
 
 
 
-if ((year >= 2010) && (year <= 2020) || (year == 2300)) { //(false) || (year == 2300)
-    alert('Вы правы!');
-} else if (year <= 1800 || year > 2300) {
-    alert('Холодно мудак!');
-} else if (isNaN(year)) {
-    alert('Введи число животное!');
-} else {
-    alert('Учи джес Гавно!');
-}
+// if ((year >= 2010) && (year <= 2020) || (year == 2300)) { //(false) || (year == 2300)
+//     alert('Вы правы!');
+// } else if (year <= 1800 || year > 2300) {
+//     alert('Холодно мудак!');
+// } else if (isNaN(year)) {
+//     alert('Введи число животное!');
+// } else {
+//     alert('Учи джес Гавно!');
+// }
 
 let myNum = 10;
 
 for (let i = 0; myNum > i; i++) {
     console.log(year = +year + i)
 }
+
+let i = 3;
+
+while (i) {
+    console.log( i-- );
+}
+
+
 
 // console.log(isNaN(year))
 
