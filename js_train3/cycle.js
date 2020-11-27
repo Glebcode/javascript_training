0e167140c2c0df32035b632448139aa173948d00
Gleb Stachev <stachev.gleb@gmail.com>
2020-11-27 15:51:16 +0200

    update(cycles):add(file and examples)

---
+++ js_train3/cycle.js
@@ -0,0 +1,35 @@
+// let b=10;
+// for (let i=2;i<=b;i++){
+//     if(i%2==0){
+// alert(i);
+//     }
+// }
+
+// let i=0;
+// while (i < 3) {
+//     alert( `number ${i}!` );
+//     i++;
+//   }
+
+// let inputNum = prompt('Please, insert number ...', 'mone than 100');
+// let i = inputNum;
+// while (i >=100){
+//     alert(`right number ${i}`);
+//     if(i < 100){
+//         let inputNum = prompt('Please, insert number ...', 'mone than 100');
+//     }
+// } 
+
+// for ( let i = prompt('Please, insert number ...', 'mone than 100'); i < 100;){
+//     let a = prompt('Please, insert number, pay attention to the condition ...', 'mone than 100');
+//    if (a >= 100){
+//     alert(`right number ${a}`);
+//    }
+// }
+
+// for (let i = 2; i < 10; i++){
+//     for (let j = 2; j < i; j++)
+//     if(i % j == 0){
+//     }
+//     alert(`Prime number - ${i}`)
+// }
